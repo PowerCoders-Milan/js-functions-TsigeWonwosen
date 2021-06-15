bd95d002175a96fbf6fd79f4ad904347a3297911
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-15 17:30:48 +0200

    exercise 4 done

---
+++ 04-convert_to_binary/decimal_to_binary.js
@@ -0,0 +1,9 @@
+
+const decimalToBin = (number)=> {
+    return number.toString(2)
+}
+
+console.log(`Decimal to Binary  4  => ${decimalToBin(4)}`)
+console.log(`Decimal to Binary  8  => ${decimalToBin(8)}`)
+console.log(`Decimal to Binary  15  => ${decimalToBin(15)}`)
+console.log(`Decimal to Binary  16  => ${decimalToBin(16)}`)
