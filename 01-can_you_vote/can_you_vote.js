4441a9db8e8f3700300573a4c5121690bb08c061
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-15 16:56:54 +0200

    exercise 1 done.

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -1,15 +1,20 @@
 // define a function that will check if I can vote or not
 const canIVote = age => {
     // define a variable legalAge
-
+const legalAge = 18;
     // create if else statement that console.log() if you can vote or not
-    if (condition) {
+    if (age >= legalAge) {
+        console.log("You can Vote!")
         
     } else {
-        
+        console.log("You are not litigable to Vote")
     }
 }
 
 // define a variable for your age
 
 // call the function and see if you can vote
+
+canIVote (20);
+console.log("******")
+canIVote (17);
