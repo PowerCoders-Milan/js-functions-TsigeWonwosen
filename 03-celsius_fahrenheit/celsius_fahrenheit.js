cc43897e17f1098d5419a745caf41e833ab43fc4
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-06-15 17:26:11 +0200

    exercise 3 done

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -2,11 +2,31 @@
 
     //return number*(9/5)
 
+const multiplyByNineFifths = (number) => {
+    return number * (9/5); 
+}
+
+
+
 // define a function getFahrenheit(celsius)
 
     // return multiplyByNineFifths(celsius) + 32;
 
-console.log() // call getFahrenheit();
+
+const getFahrenheit = (celsius) =>  {
+
+    return multiplyByNineFifths (celsius) + 32;
+}
+
+
+console.log(getFahrenheit(36)) // call getFahrenheit();
 
 
 // do the same thing starting from Fahrenheit to Celsius
+
+const toCelsius = (number) => {
+    return (number - 32 ) * (5/9)
+}
+
+console.log(`Fahrenheit to Celsius ${getFahrenheit(36)} :  ${toCelsius(getFahrenheit(36))}`)
+
